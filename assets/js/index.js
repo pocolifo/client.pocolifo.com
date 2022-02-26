a24f00d6a6ab970a94cce4a2043c42558d9ed7da
youngermax <60242131+YoungerMax@users.noreply.github.com>
2022-02-26 16:14:23 -0500

    Various updates to the site

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,16 +2,22 @@
 
 const NAV = document.querySelector('nav');
 const BACKGROUND_IMG = document.querySelector('.background-image');
+const BACKGROUND_IMG_LDR = document.querySelector('.background-image-loader');
 const NAV_LOGO = NAV.querySelector('.logo');
 
 document.addEventListener('scroll', e => {
     NAV.style.background = "rgb(33, 33, 33, " + window.scrollY / 650 + ")";
-    NAV.style.fontSize = Math.max(23, 25 - window.scrollY / 200) + "px";
+    NAV.style.fontSize = Math.max(20, 25 - window.scrollY / 200) + "px";
 
-    NAV_LOGO.style.width = Math.max(43, 48 - window.scrollY / 200) + "px";
+    NAV_LOGO.style.width = Math.max(40, 48 - window.scrollY / 200) + "px";
     BACKGROUND_IMG.style.top = "-" + window.scrollY / 6 + "px";
 });
 
-window.addEventListener('DOMContentLoaded', e => {
+window.addEventListener('load', e => {
     BACKGROUND_IMG.src = "assets/img/splash/" + Math.round(Math.random() * 4) + ".png";
+
+    BACKGROUND_IMG.onload = () => {
+        BACKGROUND_IMG.classList.add('loaded');
+        BACKGROUND_IMG_LDR.classList.add('loaded');
+    };
 });
